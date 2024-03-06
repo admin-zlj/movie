1cb8e775ef3b02c33f01c11d53d5ec6f96bb3484
admin_zlj <1759629281@qq.com>
2024-03-07 01:16:59 +0800

    feat:[demo]

--- code/demo/demo.js
+++ code/demo/demo.js
@@ -1,27 +1,28 @@
-Array.prototype.myReduce = function (callback, initValue) {
-  let len = this.length;
-  let pre = initValue || this[0];
-  let index = 0;
-  if (initValue === undefined) {
-    index = 1;
-  }
-  while (index < len) {
-    pre = callback(pre, this[index], index, this);
-    index++;
-  }
-  return pre;
-};
+['image', 'list', 'text']
 
+['image list text'];
+[
+	'image text',
+	'image list'
+]
 
+[
+	'text image',
+	'list image'
+]
 
-let res = [1, 2, 3].myReduce((pre, cur) => {
-  console.log('pre,cur', pre, cur);
-  return pre + cur;
-});
-console.log('res', res);
-
-let r = [1, 2, 3].reduce((pre, cur) => {
-  console.log('pre,cur', pre, cur);
-  return pre + cur;
-});
-console.log('res', r);
+[
+	'image image',
+	'list text'
+]
+[
+	
+	'list text',
+	'image image'
+]
+const a ={
+	temp:['image', 'list', 'text'],
+	image:{}, //image 信息
+	list:[], //list 信息
+	text:{}, //text 信息
+}
